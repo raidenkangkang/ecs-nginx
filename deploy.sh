0f9116144bbb6c8239654e726b1e33f766341361
raidenkangkang@sina.com <mrraiden8223>
2018-07-09 23:32:16 +0800

    ok

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,7 @@ aws ecs register-task-definition --family raiden-ecs-task-nginx --cli-input-json
 
 # Update the service with the new task definition and desired count
 echo "running revision"
-#REVISION=`aws ecs describe-task-definition --task-definition raiden-ecs-task-nginx | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
-REVISION="7"
+REVISION=`aws ecs describe-task-definition --task-definition raiden-ecs-task-nginx | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
 
 
@@ -28,7 +27,7 @@ if [ "$SERVICES" == "" ]; then
   if [ ${DESIRED_COUNT} = "0" ]; then
     DESIRED_COUNT="1"
   fi
-  aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT} --deployment-configuration maximumPercent=100,minimumHealthyPercent=0
+  aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:9 --desired-count ${DESIRED_COUNT} --deployment-configuration maximumPercent=100,minimumHealthyPercent=0
 else
   echo "entered new service"
   aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${TASK_FAMILY} --cluster ${CLUSTER} --region ${REGION}
