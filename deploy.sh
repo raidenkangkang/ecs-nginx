a2d420ced4b19e96f3383af0064cb7f5cca011dc
raidenkangkang@sina.com <mrraiden8223>
2018-07-09 23:01:31 +0800

    ok

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,20 @@ IMAGE_VERSION="v_"${BUILD_NUMBER}
 TASK_FAMILY="raiden-ecs-task-nginx"
 
 # Create a new task definition for this build
+echo "running sed command"
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" raiden-ecs-task-nginx.json > raiden-ecs-task-nginx-v_${BUILD_NUMBER}.json
 
+echo "running ecs register-task-definition"
 aws ecs register-task-definition --family raiden-ecs-task-nginx --cli-input-json file://raiden-ecs-task-nginx-v_${BUILD_NUMBER}.json
 
 # Update the service with the new task definition and desired count
+echo "running revision"
 REVISION=`aws ecs describe-task-definition --task-definition raiden-ecs-task-nginx | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
 
 
 #Create or update service
+echo "running if-else"
 if [ "$SERVICES" == "" ]; then
   echo "entered existing service"
   DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .services[].desiredCount`
